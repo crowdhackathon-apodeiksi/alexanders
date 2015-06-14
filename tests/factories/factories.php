246fadefe8932b4d61527b1ed24484a708185c26
alex <kyrenator@gmail.com>
2015-06-14 18:47:57 +0300

    new tests

--- tests/factories/factories.php
+++ tests/factories/factories.php
@@ -2,9 +2,9 @@
 $factory('App\Receipt', [
     'aa' => $faker->numberBetween(10000, 1000000),
     'afm' => $faker->numberBetween(100000000, 999999999),
-    'eponimia' => $faker->name,
+    'eponimia' => array_rand(['Μασούτης', 'Μαρινόπουλος', 'Καφετέρια'], 1),
     'poso' => $faker->randomFloat(2, 1, 5000),
-    'image' => $faker->imageUrl(),
+    'image' => array_rand(['http://www.xblog.gr/wp-content/uploads/2008/05/apodeiksi.jpg', 'http://www.protothema.gr/files/1/2014/07/28/apodeiksinolo123visi.jpg', 'https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcTBQ3IDHuRzS3wJfN0G-afhmPw-PcKuFfsWeFIzKJikVQZKENttbA'],1),
     'printed_at' => $faker->dateTime(),
     'user_id' => 1
 ]);
